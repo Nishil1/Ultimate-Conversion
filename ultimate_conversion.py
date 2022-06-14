34fdfebc613a3592ebd33485ae03e85742c1b259
Nishil1 <100240793+Nishil1@users.noreply.github.com>
2022-06-14 14:33:37 +1200

    Update ultimate_conversion.py

--- ultimate_conversion.py
+++ ultimate_conversion.py
@@ -1,7 +1,27 @@
-print("**** Ultimate Conversion Calculator ****")
+    print("**** Ultimate Conversion Calculator ****")
 
-Ask = int(input("Amount of measurement: "))
+    instructions = "You can convert unit of measurements between distance time and weights"
 
-unit = input("What unit would you like to use? ")
+    Ask = int(input("Amount of measurement: "))
+
+
+
+    unit = input("What unit would you like to use? ")
+
+    if unit = "mm"
+    print("you chose milimeters")
+convert = input("What unit would you like to convert to?")
+
+if convert == "km"
+print
+
+
+
+
+
+
+
+
+
 
 
